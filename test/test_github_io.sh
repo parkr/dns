aec2fe07e02357d28bb4adab0a1e0b3bd210c4cf
Parker Moore <parkrmoore@gmail.com>
2018-10-30 11:34:03 -0400

    Help.

--- test/test_github_io.sh
+++ test/test_github_io.sh
@@ -4,6 +4,11 @@ echo "--> Using curl"
 curl --version
 echo ; echo
 
+echo "--> Using egrep"
+egrep --version
+egrep --help
+echo ; echo
+
 for domain in $(script/list-domains | grep CNAME | grep '\-> parkr.github.io' | awk '{print $2}'); do
   echo "--> Testing $domain"
   echo "---------------------------------"
@@ -12,7 +17,7 @@ for domain in $(script/list-domains | grep CNAME | grep '\-> parkr.github.io' | 
   trap 'rm $outfile' EXIT
   time curl -sSfLv "https://$domain" > /dev/null 2>"$outfile"
 
-  egrep '< HTTP/\d(\.\d)? 200( OK)?' "$outfile" || {
+  egrep '< HTTP/\d(\.\d)? 200' "$outfile" || {
     cat "$outfile"
     echo " !!! Not a 200"
     exit 1
