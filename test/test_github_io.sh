6a1a1be036b5eaa80575a84375b85a7b57aaff4b
Parker Moore <parkrmoore@gmail.com>
2018-10-30 11:31:26 -0400

    Write body to dev null

--- test/test_github_io.sh
+++ test/test_github_io.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 
+echo "--> Using curl"
+curl --version
+echo ; echo
+
 for domain in $(script/list-domains | grep CNAME | grep '\-> parkr.github.io' | awk '{print $2}'); do
   echo "--> Testing $domain"
   echo "---------------------------------"
 
   outfile=$(mktemp)
   trap 'rm $outfile' EXIT
-  time curl -sSfLv "https://$domain" > "$outfile" 2>&1
+  time curl -sSfLv "https://$domain" > /dev/null 2>"$outfile"
 
-  grep "< HTTP/2 200" "$outfile" || {
+  egrep '< HTTP/\d(\.\d)? 200( OK)?' "$outfile" || {
     cat "$outfile"
     echo " !!! Not a 200"
     exit 1
