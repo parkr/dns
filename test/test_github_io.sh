9c969d034068dcf60933c811cffdf3081a467aa3
Parker Moore <parkrmoore@gmail.com>
2018-10-30 11:18:57 -0400

    Try this a bit differently

--- test/test_github_io.sh
+++ test/test_github_io.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
 
 for domain in $(script/list-domains | grep CNAME | grep '\-> parkr.github.io' | awk '{print $2}'); do
-  echo "Testing $domain"
-  resp=$(curl -sSfLv "https://$domain" 2>&1)
+  echo "--> Testing $domain"
+  echo "---------------------------------"
 
-  echo "$resp" | grep "< HTTP/2 200" || {
+  outfile=$(mktemp)
+  trap 'rm $outfile' EXIT
+  time curl -sSfLv "https://$domain" > "$outfile" 2>&1
+
+  grep "< HTTP/2 200" "$outfile" || {
+    cat "$outfile"
     echo " !!! Not a 200"
     exit 1
   }
+
+  echo ; echo;
 done
